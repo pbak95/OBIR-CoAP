85be4da1a50d166be3e245a9744e199ecab2fc4d
michakrzeminski <micha.krzeminski@gmail.com>
2017-04-28 14:44:25 +0200

    send functions declaration

--- CoAP_Server/CoAP_Server.ino
+++ CoAP_Server/CoAP_Server.ino
@@ -112,6 +112,11 @@ void loop(void) {
        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
     */
 
+    //flag to indicate if coap field is handled
+    bool flag = false;
+    //flag to send back to client diagnostic payload
+    bool diagnostic_payload = false;
+
     //header is first 4B of coap message
     int header_length = 4;
 
@@ -124,10 +129,13 @@ void loop(void) {
     if(T == 1)
     {
       Serial.println("Type: NON");
+      flag = true;
     }
     else
     {
       Serial.println("Type: other");
+      flag = true;
+      diagnostic_payload = true;
     }
 
     //field token length 0-255
@@ -147,6 +155,9 @@ void loop(void) {
     Serial.print(".");
     Serial.println(code_detail, DEC);
 
+    //type of request
+    int request_type;
+
     switch(code_class)
     {
       case 0:
@@ -156,15 +167,18 @@ void loop(void) {
        {
         //GET
         Serial.println("GET request");
+        request_type = 1;
        }
        else if(code_detail == 3)
        {
         //PUT
         Serial.println("PUT request");
+        request_type = 3;
        }
        else
        {
         Serial.println("Different request");
+        diagnostic_payload = true;
        }
        break;
       }
@@ -172,18 +186,23 @@ void loop(void) {
       {
         //success response
         Serial.println("success response");
+        flag = false;
         break;
       }
       case 4:
       {
         //client error response
         Serial.println("client error response");
+        flag = true;
+        diagnostic_payload = true;
         break;
       }
       case 5:
       {
         //server error response
         Serial.println("server error response");
+        flag = true;
+        diagnostic_payload = true;
         break;
       }
     }
@@ -207,6 +226,8 @@ void loop(void) {
 
     //until byte is not flag 11111111
     byte options[MAX_BUFFER];
+    char * uri_path_option;
+    int resource_id;
 
     //number that indicates option
     uint8_t option_no = 0;
@@ -232,31 +253,70 @@ void loop(void) {
         case 4:
         {
           Serial.println("ETag option");
+          byte etag_option[opt_length];
+          for(int i=0;i<opt_length;++i)
+          {
+            etag_option[i]=opt_value[i];
+          }
           break;
         }
         case 11:
         {
           Serial.println("Uri-Path option");
+          uri_path_option = (char*) malloc (opt_length);
+          for(int i=0;i<opt_length;++i)
+          {
+            uri_path_option[i]=opt_value[i];
+          }
+
+          if(uri_path_option == "lamp")
+          {
+            resource_id = 0;
+          }
+          if(uri_path_option == "button")
+          {
+            resource_id = 1;
+          }
           break;
         }
         case 12:
         {
           Serial.println("Content-Format option");
+          byte content_format_option[opt_length];
+          for(int i=0;i<opt_length;++i)
+          {
+            content_format_option[i]=opt_value[i];
+          }
           break;
         }
         case 17:
         {
           Serial.println("Accept option");
+          byte acept_option[opt_length];
+          for(int i=0;i<opt_length;++i)
+          {
+            acept_option[i]=opt_value[i];
+          }
           break;
         }
         case 23:
         {
           Serial.println("Block2 option");
+          byte byte2_option[opt_length];
+          for(int i=0;i<opt_length;++i)
+          {
+            byte2_option[i]=opt_value[i];
+          }
           break;
         }
         case 28:
         {
           Serial.println("Size2 option");
+          byte size2_option[opt_length];
+          for(int i=0;i<opt_length;++i)
+          {
+            size2_option[i]=opt_value[i];
+          }
           break;
         }
       }
@@ -271,5 +331,39 @@ void loop(void) {
       payload[j] = packetBuffer[iter + j];
       Serial.println(payload[j]);
     }
+
+    //if flag=false do nothing, if flag=true send smth
+    if(flag)
+    {
+      if(diagnostic_payload)
+      {
+        //send diagnostic payload to client
+      }
+      else
+      {
+        if(uri_path_option == NULL)
+        {
+          if(resource_id != 2)
+          {
+            sendToObject(request_type, resource_id, payload);
+          }
+          else
+          {    sendToClient(resource_id);
+          }
+        }
+      }
+    }
   }
+}
+
+void sendToObject(int request_type, int resource_id, byte payload[])
+{
+  //function to send message to smart object
+  Serial.println("sending message to object");
+}
+
+void sendToClient(int resource_id)
+{
+  //function to send message to coap client
+  Serial.println("sending message to client");
 }
