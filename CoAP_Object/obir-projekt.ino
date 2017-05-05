2e4851e1bc1e19b81a2fd2b69387815904a2cfa8
tomek135 <tomekboguski@gmail.com>
2017-05-05 19:29:02 +0200

    uni-mini messages

--- CoAP_Object/obir-projekt.ino
+++ CoAP_Object/obir-projekt.ino
@@ -1,3 +1,7 @@
+#include <nRF24L01.h>
+#include <RF24_config.h>
+#include <RF24Network_config.h>
+#include <Sync.h>
 #include <RF24Network.h>
 #include <RF24.h>
 #include <SPI.h>
@@ -8,24 +12,29 @@ RF24Network network(radio);
 unsigned long default_light_state = 1000; //domyslna ustwiona wartosc lapmki
 int zmiennak = 0;
 int tab[4];
+unsigned long czas = 0;
 unsigned long start = 0;
 const uint16_t this_node = 01;
 const uint16_t other_node = 00;
 
 struct payload_t {
   uint8_t header;
-  byte payload[5];
+  uint8_t state;
+  uint32_t value;
 };
 
 void setup() {
   // put your setup code here, to run once:
+  radio.begin();
   Serial.begin(57600);
   SPI.begin();
-  radio.begin();
   network.begin(110, this_node); // 110 - wspĂłlny kanaĹ‚, 1 - identyfikator swojego wezĹ‚a
   pinMode(2, INPUT); // przycisk monostabilny
   pinMode(3, OUTPUT); // lampka
-  analogWrite(3,  255 - default_light_state * 255 / 1000);
+  Serial.println("jest ok ");
+  Serial.println(255 - default_light_state * 255 / 1000);
+  Serial.println("jest ok ");
+  Serial.println(255 - default_light_state * 255 / 1000);
 }
 
 void loop() {
@@ -35,7 +44,6 @@ void loop() {
   int i = 0;
   while (Serial.available())
   {
-
     tab[i] = Serial.read();
     tab[i] = tab[i] - 48;
     zmiennak += tab[i] * pow(10, (3 - i));
@@ -50,17 +58,20 @@ void loop() {
   }
   zmiennak = 0;
 
-
   if (digitalRead(2) == LOW)
   {
     //włącznik jest nacisniety
     start = millis();
+    czas = 0;
     digitalWrite(2, HIGH);
+    Serial.println("HIGh ") ;
+    delay(300);
   }
   else
   {
     //włącznik jest zwolniony
     digitalWrite(2, LOW);
+    delay(300);
   }
 
   // Receive Message
@@ -74,11 +85,8 @@ void loop() {
     Serial.print("Operation: ") ;
     uint8_t head = payload_r.header;
     uint8_t operation = payload_r.header >> 6;
-    uint8_t resource = payload_r.header && 63;
-    byte button_position = payload_r.payload[0];
-    byte light_state[2];
-    light_state[0] = payload_r.payload[3];
-    light_state[1] = payload_r.payload[4];
+    uint8_t resource = payload_r.header & 63;
+    uint32_t light_state = payload_r.value;
     payload_t payload_s;
     ////GET
     if (operation == 0)
@@ -91,12 +99,10 @@ void loop() {
       if (resource == 0) // pobieramy aktualny stan jasnosci lapmki
       {
         Serial.println(resource);
-        Serial.print("Odebrane z urzadzenia: ");
-        Serial.println(light_state[0] << 8 + light_state[1]);
-        payload_s.payload[3] = default_light_state >> 8 && 255;
-        payload_s.payload[4] = default_light_state && 255;
+        light_state = default_light_state;
         Serial.print("Light state: ");
         Serial.println(default_light_state);
+
       }
       ////Przycisk
       else if (resource == 1)
@@ -105,32 +111,28 @@ void loop() {
         if (digitalRead(2) == LOW)
         {
           //wlacznik jest nacisniety
-          button_position = 255;
+          payload_s.state = 255;
           Serial.print("Button state: ");
-          Serial.println(button_position);
+          Serial.println(payload_s.state);
           digitalWrite(2, HIGH);
         }
         else
         {
           //wlacznik jest zwolniony
-          button_position = 0;
+          payload_s.state = 0;
           Serial.print("Button state: ");
-          Serial.println(button_position);
+          Serial.println(payload_s.state);
           digitalWrite(2, LOW);
         }
-        unsigned long czas = millis() - start;
+        czas = millis() - start;
         Serial.print("start =  ");
         Serial.println(start);
         Serial.print("Button time: ");
         Serial.println(czas);
-        payload_s.payload[1] = czas >> 24 && 255;
-        payload_s.payload[2] = czas >> 16 && 255;
-        payload_s.payload[3] = czas >> 8 && 255;
-        payload_s.payload[4] = czas && 255 ;
+        light_state = czas;
       }
-
-      payload_s.header = operation << 6 || resource;
-      payload_s.payload[0] = button_position;
+      payload_s.header = head;
+      payload_s.value = light_state;
       RF24NetworkHeader header(other_node);
       bool ok = network.write(header, &payload_s, sizeof(payload_s));
       if (ok)
@@ -143,22 +145,20 @@ void loop() {
     ////PUT
     else if ( operation == 1)
     {
-      Serial.print(" PUT");
-      Serial.println("Header: ");
-      Serial.print(payload_r.header);
-      Serial.println("Resource: ");
+      Serial.println("PUT");
+      Serial.print("Header: ");
+      Serial.println(payload_r.header);
+      Serial.print("Resource: ");
 
-      if (resource == 0) // ustawiamy poziom jasnosci w taki jaki dostalismy 
+      if (resource == 0) // ustawiamy poziom jasnosci w taki jaki dostalismy
       {
         Serial.println(resource);
         Serial.print("Odebrane z urzadzenia: ");
-        Serial.println(light_state[0] << 8 + light_state[1]);
-        default_light_state = light_state[0] << 8 + light_state[1];
-        analogWrite(3, default_light_state);
-        payload_s.payload[3] = default_light_state >> 8 && 255;
-        payload_s.payload[4] = default_light_state && 255;
+        Serial.println(light_state);
+        default_light_state = light_state;
+        analogWrite(3, 255 - default_light_state * 255 / 1000);
         Serial.print("Light state: ");
-        Serial.println(default_light_state);
+        Serial.println(light_state);
       }
       else
       {
@@ -167,4 +167,3 @@ void loop() {
     }
   }
 }
-
